7c0fda5834036c1a6d7cdae075a938a91fd1008b
Anthony Bragg <braggac21@gmail.com>
2022-06-21 16:31:49 +0200

    PS-41 Created a test for decrement function

--- transactions.js
+++ transactions.js
@@ -9,3 +9,26 @@ class Product {
         this.quantity -= amount;
     }
 }
+
+class Sale {
+    constructor() {
+        this.productList = [new Product("Apple",3.00,5.00), new Product("Orange",3.00,5.00), new Product("Banana",3.00,5.00), 
+                new Product("Pear",3.00,5.00), new Product("Grape Fruit",3.00,5.00), new Product("Fuji Apple",3.00,5.00),
+                new Product("Honey Crisp Apple",3.00,5.00), new Product("Yams",3.00,5.00), new Product("White Grapes",3.00,5.00),
+                new Product("Raspberry",3.00,5.00), new Product("Black Berry",3.00,5.00), new Product("Cherry",3.00,5.00),
+                new Product("Strawberry",3.00,5.00), new Product("Potato",3.00,5.00), new Product("Carrot",3.00,5.00),
+                new Product("Tomato",3.00,5.00), new Product("Cucumber",3.00,5.00), new Product("Radish",3.00,5.00),
+                new Product("Cherry Tomato",3.00,5.00), new Product("Jalapeno",3.00,5.00), new Product("Melon",3.00,5.00)];
+    }
+}
+
+function main() {
+    let testie = new Sale();
+    let header = document.getElementById("feed");
+    let stringy = "";
+    testie.productList[0].decrement(4.0);
+    for (let i = 0; i < 21; i++){
+        stringy += testie.productList[i]["quantity"] + " ";
+    }
+    header.innerHTML = stringy;
+}
