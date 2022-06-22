5bd727526f2adb7da96a196436c57c1f039b45a7
Anthony Bragg <braggac21@gmail.com>
2022-06-22 11:38:33 +0200

    button generation grid functional -Anthony

--- ButtonGrid.js
+++ ButtonGrid.js
@@ -1,30 +1,52 @@
+var pageNumber;
+var testInventory;
+
+function onloadInitialize() {
+    pageNumber = 0; 
+    testInventory = new CurrentInventory();
+    generateButtonGrid(0);
+}
+
+
 function createButton(product, buttonGrid) {
     var button = document.createElement("button");
     button.setAttribute("type", "button");
     button.setAttribute("class", "col-md-4 col-6 btn btn-primary");
 
-    button.innerHTML = "test";
+    button.innerHTML = product["name"];
+
+    button.onclick = function(){
+        document.getElementById("test").append(product["name"]);
+    }
 
     buttonGrid.append(button);
 }
 
-function generateButtonGrid(page) {
+function decrementPage() {
+    pageNumber--;
+    if (pageNumber < 0) {
+        pageNumber = 0;
+    }
+}
+
+function incrementPage() {
+    pageNumber++;
+    if (pageNumber > parseInt((testInventory.numberOfElements-1)/15)) {
+        pageNumber = parseInt((testInventory.numberOfElements-1)/15);
+    }
+}
+
+function generateButtonGrid() {
     var buttonGrid = document.getElementById("buttonGrid");
     buttonGrid.innerHTML = "";
 
-    createButton("A", buttonGrid);
-    createButton("A", buttonGrid);
+    var maximumIndex = (pageNumber + 1) * 15;
 
-    /*
-    var testInventory = new CurrentInventory();
-    var maximumIndex = (page + 1) * 15;
-
-    if (maximumIndex < testInventory.numberOfElements) {
+    if (maximumIndex > testInventory.numberOfElements) {
         maximumIndex = testInventory.numberOfElements;
     }
 
-    for (let i = (page*15); i < maximumIndex; i++) {
-        
+    for (let i = (pageNumber*15); i < maximumIndex; i++) {
+        createButton(testInventory.productList[i], buttonGrid);
     }
-    */
 }
