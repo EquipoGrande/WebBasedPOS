30d977a72f73a59bb71322c4cd587b658ff3a50b
Chris Weeks <chrisrweeks@aol.com>
2022-06-28 02:37:28 +0200

    added getproductbyid

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,29 @@ module.exports = function (express) {
     var router = express.Router();
 
     var db = require('../Database');
-    //const router = require('../')();
 
+    /**
+     * Returns a list of all products in JSON format
+     */
     router.get('/getproductslist', (req, res) => {
-        console.log("test")
         db.query('SELECT * FROM product',(err, queryResult)=>{
             if (!err) {
             res.send(queryResult.rows)
             } else {
             console.log(err.message);
+            }
+        });
+    });
+
+    /**
+     * Returns the product specified by the product id in JSON format
+     */
+     router.get('/getproductbyid', (req, res) => {
+        db.query('SELECT * FROM product WHERE productid=' + req.query.productid, (err, queryResult) => {
+            if(!err) {
+                res.send(queryResult.rows);
+            } else {
+                console.log(err.message);
             }
         });
     });
