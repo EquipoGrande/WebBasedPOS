c390bec7c912f77284a95e931dfe34f3c3f306f9
Chris Weeks <chrisrweeks@aol.com>
2022-06-28 09:40:50 +0200

    restructured directories

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = require('express')();
 const router = express.Router();
 const port = 3000;
 const { application_name } = require('pg/lib/defaults');
+const path = require('path');
 
 
 // Landing page is login page
@@ -16,6 +17,9 @@ var productListController = require('./controllers/products')(express);
 app.use('/api/products', productListController);
 
 // serve all the static files. They will match the folder structure of this project
-app.use(express.static('./'));
+app.use('/', express.static(path.join(__dirname, 'html')));
+app.use('/', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
