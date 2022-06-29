0cac58655afce7bcbdbd33ba693d3bef11eed9ef
Anthony Bragg <braggac21@gmail.com>
2022-06-29 18:30:25 +0200

    created restock report API functionality - Anthony

--- controllers/salesTrends.js
+++ controllers/salesTrends.js
@@ -5,6 +5,13 @@ const salesReportQuery = "SELECT productName, sum(quantity) as amountSold, sum(q
 
 const dateRangeQuery = "SELECT MIN(saleID), MAX(saleID) FROM saleHistory WHERE saleDate BETWEEN $1 AND $2;"
 
+const restockReportQuery =  "SELECT productName,  stockquantity, amountSold, revenue FROM ("
+    + "SELECT productName, stockquantity, sum(quantity) as amountSold, sum(quantity*sellprice) as revenue " 
+    + "FROM Product p, currentinventory c, salelineitem s "
+    + "WHERE s.productID = p.productID AND c.productID = p.productID AND s.saleID BETWEEN $1 AND $2 "
+    + " GROUP BY p.productName, stockQuantity"
+    + ") AS SalesReport WHERE amountSold > stockQuantity;"
+
 var idRange;
 
 //
@@ -44,7 +51,7 @@ module.exports = function (express) {
         let dateRange = req.body;
         await getIDs(dateRange[0], dateRange[1], db).then();
 
-        db.query(salesReportQuery, [idRange.min, idRange.max])
+        db.query(restockReportQuery, [idRange.min, idRange.max])
         .then(queryResult => {
             res.status(200);
             console.log(queryResult.rows);
