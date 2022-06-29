fd8a5ef8c1c6eff04a88705e78e4f98550569cc2
Anthony Bragg <braggac21@gmail.com>
2022-06-29 17:50:23 +0200

    created Sales report API functionality - Anthony

--- controllers/salesTrends.js
+++ controllers/salesTrends.js
@@ -3,12 +3,19 @@ const salesReportQuery = "SELECT productName, sum(quantity) as amountSold, sum(q
     + "FROM Product p, salelineitem s WHERE s.productID = p.productID AND s.saleID BETWEEN $1 AND $2 "
     + "GROUP BY p.productName;";
 
+const dateRangeQuery = "SELECT MIN(saleID), MAX(saleID) FROM saleHistory WHERE saleDate BETWEEN $1 AND $2;"
 
-const sqlGetRange = "SELECT MIN(saleID), MAX(saleID) FROM saleHistory WHERE saleDate BETWEEN $1 AND $2;";     
+var idRange;
+
 //
-async function getIDs(start, end) {
-    let idRange = await db.query(sqlGetRange).then(result => result.rows[0]);
-    return idRange;
+async function getIDs(start, end, db) {
+    startDate = Date.parse(start);
+    endDate = Date.parse(end);
+    idRange = await db.query(dateRangeQuery, [start, end])
+        .then(result => result.rows[0])
+        .catch(err => {
+            console.log(err.message);
+        });
 }
 
 module.exports = function (express) {
@@ -16,7 +23,39 @@ module.exports = function (express) {
 
     var db = require('../Database');
 
-    router.get('/salesReport', async (req, res) => {)
+    router.get('/salesReport', async (req, res) => {
+        let dateRange = req.body;
+        await getIDs(dateRange[0], dateRange[1], db).then();
+
+        db.query(salesReportQuery, [idRange.min, idRange.max])
+        .then(queryResult => {
+            res.status(200);
+            console.log(queryResult.rows);
+            res.send(queryResult.rows);
+        })
+        .catch(err => {
+            res.status(500);
+            console.log(err.message);
+            res.send(err.message);
+        })
+    })
+
+    router.get('/restockReport', async (req, res) => {
+        let dateRange = req.body;
+        await getIDs(dateRange[0], dateRange[1], db).then();
+
+        db.query(salesReportQuery, [idRange.min, idRange.max])
+        .then(queryResult => {
+            res.status(200);
+            console.log(queryResult.rows);
+            res.send(queryResult.rows);
+        })
+        .catch(err => {
+            res.status(500);
+            console.log(err.message);
+            res.send(err.message);
+        })
+    })
 
 
     return router;
---
+++ js/trendLogic.js
@@ -0,0 +1,9 @@
+function onloadTrends() {
+    
+}
+
+function startDate() {
+    console.log(document.getElementById("startDate").value);
+}
+
+window.addEventListener('load', onloadTrends);
