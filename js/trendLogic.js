e666b2b29d5e6aa2a853e3aad5e1854d3bc06ea2
Anthony Bragg <braggac21@gmail.com>
2022-06-29 19:10:47 +0200

    Created javascript interface for salesreport

--- js/trendLogic.js
+++ js/trendLogic.js
@@ -1,9 +1,24 @@
+const { response } = require("express");
+
 function onloadTrends() {
     
 }
 
-function startDate() {
-    console.log(document.getElementById("startDate").value);
+async function generateSalesReport() {
+    salesReport = await fetchReport('http://localhost:3000/api/trends/salesReport?start=').then(response => response.json());
+    console.log(salesReport);
+}
+
+async function fetchReport(urlQuery) {
+    startDate = document.getElementById("startDate").value;
+    endDate = document.getElementById("endDate").value;
+
+    if (startDate == null || endDate == null) {
+        // implement error here
+        return
+    }
+
+    return fetch(urlQuery + startDate + "&end=" + endDate, {method: 'GET'});
 }
 
 window.addEventListener('load', onloadTrends);
