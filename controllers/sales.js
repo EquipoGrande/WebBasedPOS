8f0925eb3920d7d5f2a2c36525baad6399142c41
Anthony Bragg <braggac21@gmail.com>
2022-06-29 10:54:20 +0200

    Make sale now updates the inventory -Anthony

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,6 +1,8 @@
 const sqlGetNextID = 'SELECT MAX(saleid) as max FROM salehistory;';
 const sqlLineItem = "INSERT INTO salelineitem VALUES($1, $2, $3);";
 const sqlHistoryEntry = "INSERT INTO salehistory VALUES($1, $2, $3);";
+const sqlUpdateInventory = "UPDATE currentinventory SET stockQuantity = $1 WHERE productID = $2";
+const sqlCheckInventory = "SELECT stockQuantity FROM currentinventory WHERE productID = $1";
 
 var max_id;
 
@@ -26,8 +28,21 @@ module.exports = function (express) {
         var total = 0.0;
         var date = new Date();
 
+        
+
         for (let i = 0; i < saleList.length; i++) {
             await db.query(sqlLineItem, [max_id, saleList[i].productID, saleList[i].quantity]);
+           
+        
+            let currentInventory = await db.query(sqlCheckInventory, [saleList[i].productID]).then(result => result.rows[0].stockquantity);
+            var updateInventory =  parseFloat(currentInventory) - parseFloat(saleList[i].quantity);
+
+            if (updateInventory < 0) {
+                updateInventory = 0.0;
+            }
+
+            await db.query(sqlUpdateInventory, [parseFloat(updateInventory), saleList[i].productID]);
+
             total = total + parseFloat(saleList[i].sellprice);
         }
 
