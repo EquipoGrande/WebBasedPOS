1a366820c6a3d3863444d4762d0100bffde1605e
carolineemejia <hi123484@tamu.edu>
2022-06-29 17:29:59 +0200

    Added queries for manager vs cashier

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,16 @@
 const query_getUserInfo = `SELECT * FROM userinformation;`;
+const query_getManager = `SELECT * FROM userinformation WHERE role='Manager';`;
+const query_getCashier = `SELECT * FROM userinformation WHERE role='Cashier';`;
+
 
 module.exports = function (express) {
     var router = express.Router();
 
     var db = require('../Database');
 
+    /**
+     * Returns a list of the user information in JSON format
+     */
     router.get('/getUsers', (req, res) => {
         db.query(query_getUserInfo)
             .then(queryResult => {
