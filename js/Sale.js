d093cacde17a8f6c9c3cc86bcd10cb392fbb07c7
julianaLeano <julexd125@gmail.com>
2022-06-30 00:54:36 +0300

    updated remove button on Sale.js

--- js/Sale.js
+++ js/Sale.js
@@ -74,7 +74,12 @@ class Sale {
 
         let removeElement = document.createElement("td");
         let removeButton = document.createElement("button");
-        removeButton.innerHTML = "   X   ";
+        removeButton.innerHTML = "X";
+        removeButton.style.backgroundColor = '#8F423D'; 
+        removeButton.style.color='white'
+        removeButton.style.width='35px'
+        removeButton.style.border='#8F423D';
+
         removeButton.onclick = function() {
             newSale.removeItemByID(product["productid"]);
         }
