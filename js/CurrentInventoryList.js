0127eddc07b65d87ba4a20b1a374ef3ea696e271
Chris Weeks <chrisrweeks@aol.com>
2022-06-30 11:35:58 +0200

    added more alerts

--- js/CurrentInventoryList.js
+++ js/CurrentInventoryList.js
@@ -7,12 +7,12 @@ async function generateInventory() {
             if (getRequest.status == 200) {
                 resolve(getRequest.response);
             } else {
-                showAlert("alert-sucess", "Cannot find JSON data");
+                showAlert("alert-danger", "Cannot find JSON data");
                 reject(Error(getRequest.statusText));
             };
         }
         getRequest.onerror = function() {
-            showAlert("alert-sucess", "Cannot find JSON data");
+            showAlert("alert-danger", "Cannot find JSON data");
             reject(Error('Cannot find JSON data'));
         }
         getRequest.send();
