209f82561bbf653a493449fa0131e8e23f1d3b70
Anthony Bragg <braggac21@gmail.com>
2022-06-30 21:10:23 +0200

    Fixed Merge Issues -Anthony

--- js/trendLogic.js
+++ js/trendLogic.js
@@ -134,19 +134,6 @@ function generateTable(report, columnNames) {
 
         trendsTable.append(currentRow)
     }
-<<<<<<< HEAD
-=======
-}
-
-function isFloat(value) {
-    if (typeof value === 'number' &&
-        !Number.isNaN(value) &&
-        !Number.isInteger(value)
-    ) {
-        return true;
-    }
-    return false;
->>>>>>> c83e102104deef5fa63e2b8dfa92376faee14105
 }
 
 function getTopReport(report, sortname) {
@@ -207,7 +194,6 @@ function getTopPairs(report) {
 }
 
 function makeGraph(reportLists) {
-
     if (trendChart != null) {
         trendChart.destroy();
     }
